c8e8aa3b01418e7374feea52548c2bb74f0360ed
ar-programmer <alexander.rabau@gmail.com>
2018-05-23 18:47:05 +0200

    Persisting data using Codable protocol and Plist

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -12,10 +12,14 @@ class TodoListViewController: UITableViewController{
 
       var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        
+     
         
         let newItem = Item()
         newItem.title = "Find Mike"
@@ -29,10 +33,12 @@ class TodoListViewController: UITableViewController{
         newItem3.title = "Destroy Demogorgon"
         itemArray.append(newItem3)
         
+        loadItems()
+        
         // Do any additional setup after loading the view, typically from a nib.
         
-       if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
-            itemArray = items        }
+//       if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+//            itemArray = items        }
         
       
         
@@ -72,23 +78,10 @@ class TodoListViewController: UITableViewController{
         
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-//        itemArray[indexPath.row].done == false {
-//            itemArray[indexPath.row].done = true
-//        } else {
-//            itemArray[indexPath.row].done = false
-//        }
+
+        saveItems()
         
-        
-        
-        // ??? müssen die ausgeklammerten Zeilen weg???
-        
-//        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-//        } else {
-//            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-//        }
-        
-        tableView.reloadData()
+
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -110,9 +103,9 @@ class TodoListViewController: UITableViewController{
             
              self.itemArray.append(newItem)
             
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            
-            self.tableView.reloadData()
+            self.saveItems()
+      
+
         }
         
             alert.addTextField { (alertTextField) in
@@ -125,5 +118,36 @@ class TodoListViewController: UITableViewController{
         
         present(alert, animated: true, completion: nil)
     }
+    
+    func saveItems() {
+        
+        let encoder = PropertyListEncoder()
+        
+        do{
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("Error encoding item array, \(error)")
+            
+        }
+        
+        self.tableView.reloadData()
+        
+    }
+    
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!){
+        let decoder = PropertyListDecoder()
+        
+        
+        do{
+        
+            itemArray = try decoder.decode([Item].self, from: data)
+        } catch {
+            print("Error decoding item array, \(error)")
+        }
+    }
+}
+
 }
 
