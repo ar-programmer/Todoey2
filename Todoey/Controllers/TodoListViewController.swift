9e5d7f0821c976c72457d7980cb667734aa605e0
ar-programmer <alexander.rabau@gmail.com>
2018-05-30 07:23:22 +0200

    fixed Bug1

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -7,31 +7,40 @@
 //
 
 import UIKit
+import CoreData
 
 class TodoListViewController: UITableViewController{
+    
+
 
       var itemArray = [Item]()
     
+   // var itemArray : [Item]!
+
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
-    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+
+      
      
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
+//        let newItem = Item()
+//        newItem.title = "Find Mike"
+//        itemArray.append(newItem)
+//
+//        let newItem2 = Item()
+//        newItem2.title = "Buy Eggos"
+//        itemArray.append(newItem2)
+//
+//        let newItem3 = Item()
+//        newItem3.title = "Destroy Demogorgon"
+//        itemArray.append(newItem3)
         
         loadItems()
         
@@ -98,8 +107,12 @@ class TodoListViewController: UITableViewController{
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
            
             
-            let newItem = Item()
+            let newItem = Item(context: self.context)
+           
+            
+           
             newItem.title = textField.text!
+            newItem.done = false
             
              self.itemArray.append(newItem)
             
@@ -121,13 +134,10 @@ class TodoListViewController: UITableViewController{
     
     func saveItems() {
         
-        let encoder = PropertyListEncoder()
-        
         do{
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
+           try context.save()
         } catch {
-            print("Error encoding item array, \(error)")
+            print("Error saving context, \(error)")
             
         }
         
@@ -136,18 +146,13 @@ class TodoListViewController: UITableViewController{
     }
     
     func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!){
-        let decoder = PropertyListDecoder()
         
-        
-        do{
-        
-            itemArray = try decoder.decode([Item].self, from: data)
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+      itemArray =  try context.fetch(request)
         } catch {
-            print("Error decoding item array, \(error)")
+            print("Error fetching data from context \(error)")
         }
-    }
 }
-
 }
 
